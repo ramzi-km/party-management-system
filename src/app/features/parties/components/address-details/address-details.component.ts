a1a130fc424d09fcca04ba025afc10cd0ea9a232
Ramzi K.M <ramzikm3@gmail.com>
2024-06-14 01:20:28 +0530

    :iphone: fix responsive issue in party details page

--- src/app/features/parties/components/address-details/address-details.component.ts
+++ src/app/features/parties/components/address-details/address-details.component.ts
@@ -9,26 +9,4 @@ import { Component, Input } from '@angular/core'
 export class AddressDetailsComponent {
     @Input() addresses!: IAddress[]
 
-    addressess: IAddress[] = [
-        {
-            id: 23,
-            address_line_1: '123, Elm Street',
-            address_line_2: 'Near Central Park',
-            country: 'USA',
-            state: 'New York',
-            city: 'New York',
-            pincode: 10001,
-            is_active: true,
-        },
-        {
-            id: 34,
-            address_line_1: '456, Oak Avenue',
-            address_line_2: 'Opposite Main Square',
-            country: 'USA',
-            state: 'California',
-            city: 'Los Angeles',
-            pincode: 90001,
-            is_active: false,
-        },
-    ]
 }
--- src/app/features/parties/components/bank-details/bank-details.component.ts
+++ src/app/features/parties/components/bank-details/bank-details.component.ts
@@ -9,24 +9,4 @@ import { IBank } from './../../../../core/interfaces/bank.interface'
 export class BankDetailsComponent {
     @Input() banks!: IBank[]
 
-    bankss: IBank[] = [
-        {
-            id: 323434,
-            bank_name: 'hdfc',
-            bank_ifsc_code: '323423',
-            account_no: '2334323423423',
-            account_holder_name: 'Ramzi K.M',
-            branch_name: 'Vazhakkad',
-            is_active: true,
-        },
-        {
-            id: 323434,
-            bank_name: 'Sbi',
-            bank_ifsc_code: 'ss3dd23423',
-            account_no: '2334323423423',
-            account_holder_name: 'Ramzi K.M',
-            branch_name: 'Kondotty',
-            is_active: false,
-        },
-    ]
 }
